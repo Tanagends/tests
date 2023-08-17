e23a63b6247771336de3356064809a40b4287aea
Tanagends <tanagends@gmail.com>
2023-08-17 15:10:28 -0700

    tests

--- zimboloop.c
+++ zimboloop.c
@@ -1,29 +1,38 @@
 #include "zimbo.h"
+/**
+ * main - a simple shell
+ * Return: 0(Always success).
+ */
 //char *builtstr = {"cd", "setenv", "env", "exit"};
 //int (*builtfunc[])(char **toks) = {zimbo_cd, zimbo_setenv, zimbo_env, 
 //	zimbo_exit};
+// command prompt duplicating upon null input
+// double free error
 extern char** environ;
-int main(void)
+int main(int argc, char **argv, char **envp)
 {
 	char *input, **toks;
 	size_t size = 0, j = 0;
-	ssize_t i = write(1, "Zimboshell$", 11);
-	ssize_t k;
-	int status;
+	ssize_t i, k;
+	int status = 1;
 
-	if (i != -1)
+	while (status)
 	{
-		k = getline(&input, &size, stdin);
-		if (k != -1)
+		i = write(1, "Zimboshell$ ", 12);
+		if (i != -1)
 		{
-			toks = zimbo_split(input);
+			k = getline(&input, &size, stdin);
+			if (k != -1)
+			{
+				toks = zimbo_split(input);
+			//else
+			//	printf("Error\n");
+				status = zimbo_execute(toks);
+				free(input);
+			}
 		}
-		else
-			printf("Error\n");
+		free(toks);
 	}
-	status = zimbo_execute(toks);
-	free(toks);
-	free(input);
 	return 0;
 }
 /**
@@ -61,15 +70,16 @@ int zimbo_execute(char **toks)
 	pid_t _pid;
 	int status;
 
+
 	_pid = fork();
-	if (toks == NULL)
+	if (toks == NULL || toks[0] == NULL)
 		return (1);
-	//Child process.
+	/*Child process.*/
 	if (_pid == 0)
 	{
 		if (execve(toks[0], toks, environ) == -1)
 		{
-			perror("No such file or directory");
+			perror("-1");
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -77,6 +87,6 @@ int zimbo_execute(char **toks)
             // Parent process
 		waitpid(_pid, &status, WUNTRACED); // Wait for the child process to finish.
 	else
-		perror("fork error"); // If fork fails
+		perror("fork error"); //If fork fails
 	return (1);
 }
